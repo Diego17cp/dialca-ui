d89db58944d2d3aecebcfdab270c9c4a21a15247
Diego17 <diegoalbertocastropastor@gmail.com>
2025-08-08 22:27:02 -0500

    Add tsup configuration for bundling TypeScript files

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+    entry: ["src/index.ts"],
+    format: ["esm", "cjs"],
+    dts: true,
+    external: ["react", "react-dom"],
+    sourcemap: true,
+    clean: true,
+    injectStyle: true,
+    // css: true
+})
