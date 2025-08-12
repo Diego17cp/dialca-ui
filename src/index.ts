133c6a46e80ef3c221160aec6068538b473f9766
Diego17 <diegoalbertocastropastor@gmail.com>
2025-08-12 15:33:17 -0500

    Refactor index.ts to improve component organization and maintainability

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
+// ==================
+// Componentes
+// ==================
 export * from './components/RadioInput';
 export * from './components/InputField';
-export * from './components/CircleCharCounter'
-export * from './components/DropZone'
-export * from './components/Loader'
-export * from './components/Select'
-export * from './components/TxtArea'
-export * from './utils/classNames'
+export * from './components/CircleCharCounter';
+export * from './components/DropZone';
+export * from './components/Loader';
+export * from './components/Select';
+export * from './components/TxtArea';
+export * from './utils/classNames';
+
+// ==================
+// Tipos
+// ==================
+
+import './types.d.ts';
 
