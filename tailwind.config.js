3479c8878d9670ba3e250345eb98db856738ddb8
Diego17 <diegoalbertocastropastor@gmail.com>
2025-08-12 15:33:54 -0500

    Refactor tailwind.config.js to improve readability and maintainability by organizing content and commenting out theme configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-    content: [
-        "./src/**/*.{js,ts,jsx,tsx,mdx}",
-        "./playground/**/*.{js,ts,jsx,tsx,mdx,html}"
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
+	content: [
+		"./src/**/*.{js,ts,jsx,tsx,mdx}",
+		"./playground/**/*.{js,ts,jsx,tsx,mdx,html}",
+	],
+	// theme: {
+	// 	extend: {
+	// 		colors: {
+	// 			primary: {
+	// 				light: "#3A7DC0",
+	// 				DEFAULT: "#085691",
+	// 				dark: "#053866",
+	// 			},
+	// 			onPrimary: "#ffffff",
+	// 			secondary: {
+	// 				DEFAULT: "#d79b5f",
+	// 			},
+	// 			onSecondary: "#1F2937",
+	// 		},
+	// 	},
+	// },
+	plugins: [],
+};
