53e9cc4480275bddb5cd5eb79b1b23bd2b401dac
Diego17 <diegoalbertocastropastor@gmail.com>
2025-11-11 16:55:29 -0500

    feat: add main-scroll class to improve scrolling behavior in DocsLayout

--- web/src/app/layouts/DocsLayout.tsx
+++ web/src/app/layouts/DocsLayout.tsx
@@ -9,7 +9,7 @@ export const DocsLayout = () => {
                 <Sidebar />
                 <div className="flex-1 flex flex-col min-w-0">
                     <MobileBreadcrumb />
-                    <div className="flex-1 overflow-y-auto">
+                    <div className="flex-1 overflow-y-auto main-scroll">
                         <main className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
                             <Outlet />
                         </main>
