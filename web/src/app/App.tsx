f98ea61cc84eafa5a4e1526fe4e22773d511da8b
Diego17 <diegoalbertocastropastor@gmail.com>
2025-11-10 22:58:12 -0500

    feat: update routing structure to include WebLayout component for improved layout management

--- web/src/app/App.tsx
+++ web/src/app/App.tsx
@@ -1,22 +1,43 @@
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import { ThemeProvider } from "./providers";
 import { LazyWrapper } from "@/core/components";
 import { HomePage } from "./routes";
+import { WebLayout } from "./layouts";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<ThemeProvider>
 				<Routes>
-					<Route
-						path="/"
-						index
-						element={
-							<LazyWrapper>
-								<HomePage />
-							</LazyWrapper>
-						}
-					/>
+					<Route path="/" element={<WebLayout />}>
+						<Route
+							index
+							element={
+								<LazyWrapper>
+									<HomePage />
+								</LazyWrapper>
+							}
+						/>
+						<Route path="docs">
+							<Route
+								index
+								element={
+									<Navigate to="/docs/overview/getting-started" />
+								}
+							/>
+							<Route path="overview">
+								<Route index element={<Navigate to="/docs/overview/getting-started" />} />
+								<Route
+									path="getting-started"
+									element={
+										<LazyWrapper>
+											<div>Getting Started Page</div>
+										</LazyWrapper>
+									}
+								/>
+							</Route>
+						</Route>
+					</Route>
 				</Routes>
 			</ThemeProvider>
 		</BrowserRouter>
