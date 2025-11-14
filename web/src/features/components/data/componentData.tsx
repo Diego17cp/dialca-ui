56b47e2cabe68c7c3a647b00ef03be45f258a541
Diego17 <diegoalbertocastropastor@gmail.com>
2025-11-14 17:19:53 -0500

    feat: create componentData file to manage component data structure

---
+++ web/src/features/components/data/componentData.tsx
@@ -0,0 +1,7 @@
+import type { ComponentData } from '../types';
+import { buttonData } from './components';
+
+
+export const componentsData: Record<string, ComponentData> = {
+    'button': buttonData,
+};
