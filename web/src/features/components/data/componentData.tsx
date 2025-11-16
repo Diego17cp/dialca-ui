02bf73acb259f3bdbd4a67d6dd86c5d734cb8ebc
Diego17 <diegoalbertocastropastor@gmail.com>
2025-11-16 15:59:22 -0500

    feat: add checkbox data to componentsData export

--- web/src/features/components/data/componentData.tsx
+++ web/src/features/components/data/componentData.tsx
@@ -1,7 +1,8 @@
 import type { ComponentData } from '../types';
-import { buttonData } from './components';
+import { buttonData, checkboxData } from './components';
 
 
 export const componentsData: Record<string, ComponentData> = {
     'button': buttonData,
+    'checkbox': checkboxData
 };
