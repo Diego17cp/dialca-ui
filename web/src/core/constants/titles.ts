bfa5d3f35bb08f34c1ae24bfa05880fedbba83ad
Diego17 <diegoalbertocastropastor@gmail.com>
2025-11-17 21:34:03 -0500

    feat: add pageTitles constant for application navigation

---
+++ web/src/core/constants/titles.ts
@@ -0,0 +1,7 @@
+export const pageTitles = {
+    HOME: 'DialcaUI - Simple & Customizable React Components Library',
+    GETTING_STARTED: 'Getting Started - Overview - DialcaUI',
+    CUSTOMIZATION: 'Customization - Overview - DialcaUI',
+    THEMING: 'Theming - Overview - DialcaUI',
+    COMPONENTS_LIST: 'Components - Overview - DialcaUI',
+}
