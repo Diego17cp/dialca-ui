7fb8b6b9e89825c38edf1e21e933f7ddca7a616d
Diego17 <diegoalbertocastropastor@gmail.com>
2025-11-27 21:33:01 -0500

    feat: replace default loading fallback with LazyLoader component in LazyWrapper

--- web/src/core/components/LazyWrapper.tsx
+++ web/src/core/components/LazyWrapper.tsx
@@ -1,11 +1,20 @@
+import { Loader } from "dialca-ui";
 import { Suspense } from "react";
+
+const LazyLoader = () => (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+        <Loader
+            size="xl"
+        />
+    </div>
+)
 
 export const LazyWrapper = ({
     children,
     fallback,
 } : { children: React.ReactNode; fallback?: React.ReactNode }) => {
     return (
-        <Suspense fallback={fallback || <div>Loading...</div>}>
+        <Suspense fallback={fallback || <LazyLoader />}>
             {children}
         </Suspense>
     )
