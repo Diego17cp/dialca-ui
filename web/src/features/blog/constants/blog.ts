631e6190620706ef003b5da41693b5d4e9dc8e4a
Diego17 <diegoalbertocastropastor@gmail.com>
2025-12-12 17:34:33 -0500

    feat: update BLOG_POSTS to include DUI130Post component in content

--- web/src/features/blog/constants/blog.ts
+++ web/src/features/blog/constants/blog.ts
@@ -1,3 +1,4 @@
+import { DUI130Post } from "../components";
 import type { BlogPost } from "../types";
 
 export const BLOG_POSTS: BlogPost[] = [
@@ -8,7 +9,7 @@ export const BLOG_POSTS: BlogPost[] = [
         date: "2025-10-16",
         cardDescription: "Style changes, reduce bundle size and improvements.",
         introduction: "Dialca UI 1.3.0 just landed. This release brings several improvements including style changes, reduced bundle size, and various enhancements to improve your experience using Dialca UI in your projects.",
-        content: "",
+        content: DUI130Post(),
         imgUrl: "/favicon.webp"
-    }
+    },
 ]
